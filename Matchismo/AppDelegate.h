3373f5d8109102c6ca0014032112deb2925d537b
LiYuanhao <Steve@SteveMBP.lan>
2015-02-10 22:42:28 +0800

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by LiYuanhao on 15/2/10.
+//  Copyright (c) 2015年 LiYuanhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by LiYuanhao on 15/2/10.
+//  Copyright (c) 2015年 LiYuanhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
